12cc316209a13390ffa2bcee174c308ff134e0e8
AmmarKayali <AmmarKayali18@gmail.com>
2021-10-25 15:41:34 +0300

    Alter files

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -5,31 +5,31 @@
     <div class="nk-nav-scroll">
         <ul class="metismenu" id="menu">
             <li class="nav-label">@lang('trans.dashboard')</li>
-            <li class="mega-menu mega-menu-sm">
+            <!-- <li class="mega-menu mega-menu-sm">
                 <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                     <span class="nav-text">@lang('trans.accounts')</span>
                 </a>
-                <ul aria-expanded="false">
+                <ul aria-expanded="false"> -->
                     <li class="@if (Request::is('all-admins')) active @endif"><a
                             href="{{route('all-admins')}}">@lang('trans.admins')</a></li>
                     <li class="@if (Request::is('all-teachers')) active @endif"><a
                             href="{{route('all-teachers')}}">@lang('trans.teachers')</a></li>
                     <li class="@if (Request::is('all-students')) active @endif"><a
                             href="{{route('all-students')}}">@lang('trans.students')</a></li>
-                </ul>
-            </li>
-            <li class="mega-menu mega-menu-sm">
+                <!-- </ul>
+            </li> -->
+            <!-- <li class="mega-menu mega-menu-sm">
                 <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                     <span class="nav-text">@lang('trans.courses')</span>
                 </a>
-                <ul aria-expanded="false">
+                <ul aria-expanded="false"> -->
 
                     <li class="@if (Request::is('categories')) active @endif"><a
                             href="{{route('categories')}}">@lang('trans.courses_categories')</a></li>
                     <li class="@if (Request::is('courses')) active @endif"><a
                             href="{{route('courses')}}">@lang('trans.courses')</a></li>
-                </ul>
-            </li>
+                <!-- </ul>
+            </li> -->
 
             <li class="@if (Request::is('sessions')) active @endif"><a
                             href="{{route('sessions')}}">@lang('trans.sessions')</a></li>
